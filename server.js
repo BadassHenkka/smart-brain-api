c388a70db86df04cbbc3f180b11af408f57770fc
BadassHenkka <henri.kestio@gmail.com>
2019-02-18 17:03:01 +0200

    updated connection

--- server.js
+++ server.js
@@ -20,10 +20,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg', // pg for postgres
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : '',
-      database : 'smart-brain'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
     }
   });
 
